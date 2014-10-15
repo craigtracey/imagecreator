89e9b1fd50f1498fc85cda97e02f7d5b2dd092aa
Craig Tracey <craigtracey@gmail.com>
2014-10-15 10:09:34 -0400

    Fix packer json creation
    Previously this code had a hack in which it would get the packer config
    as a string and then lazily echo that into a remote file. This should
    be done with a local temporary directory and a put instead. Oh and make
    the json look pretty while we are at it.

--- imagecreator/image_builder.py
+++ imagecreator/image_builder.py
@@ -16,6 +16,8 @@
 
 import logging
 import os
+import shutil
+import tempfile
 import time
 
 from fabric.api import cd, env, execute, put, run, sudo
@@ -107,10 +109,16 @@ class ImageBuilder(object):
 
     @staticmethod
     def copy_files_to_builder(tempdir, packer_config, kickstart_path):
+        local_tempdir = tempfile.mkdtemp()
+        local_packer_path = os.path.join(local_tempdir, 'packer_config.json')
+        with open(local_packer_path, 'w') as f:
+            f.write(packer_config)
+
         with cd(tempdir):
             run('mkdir httpdir')
             put(kickstart_path, 'httpdir/kickstart.cfg')
-            run("echo '%s' > packer_config.json" % (packer_config))
+            put(local_packer_path, 'packer_config.json')
+        shutil.rmtree(local_tempdir)
 
     def _run_packer(self):
         execute(ImageBuilder.run_packer, self._build_tempdir,
